478236c297cdd8ab6078e164a722131049e6d296
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 09:57:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vision";
+            int age = 14;
+            bool isTall = false;
+            double weight = 25.5;
+            Console.WriteLine("Hi " + name + " , you are " + age + " years old and your weght is " + weight);
+            Console.WriteLine("True or False you are tall : " + isTall);
+            //Freeze console
+            Console.ReadLine();
+        }
+    }
+}
